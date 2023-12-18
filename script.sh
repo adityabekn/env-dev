5e7ce5c1deb3676e8168bdc25175206857cad4ec
Aditya Bekti N <34529142+adityabekn@users.noreply.github.com>
2023-12-18 13:24:12 +0700

    add script.sh for initial setup

---
+++ script.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+echo "Are you sudo?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) 
+            a=sudo
+            break;;
+        No )
+            a=
+            break;;
+    esac
+done
+
+echo "Create a non-root user?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) 
+            echo "Enter a username"
+            read name
+            $a adduser $name
+            $a usermod -aG sudo $name
+            if [ -d "~/.ssh" ]; then
+                $a cp -r ~/.ssh /home/$name
+                $a chown -R $name:$name /home/$name/.ssh
+            fi
+            break;;
+        No ) exit;;
+    esac
+done
+
+#SSH
+$a sed -re 's/^(\#)(PermitRootLogin)([[:space:]]+)(.*)/\2\3\4/' -i /etc/ssh/sshd_config
+$a sed -re 's/^(\#)(PasswordAuthentication)([[:space:]]+)(.*)/\2\3\4/' -i /etc/ssh/sshd_config
+$a sed -re 's/^(\#)(MaxAuthTries)([[:space:]]+)(.*)/\2\3\4/' -i /etc/ssh/sshd_config
+$a sed -re 's/^(\#)(LoginGraceTime)([[:space:]]+)(.*)/\2\3\4/' -i /etc/ssh/sshd_config
+$a sed -re 's/^(\#)(PermitEmptyPasswords)([[:space:]]+)(.*)/\2\3\4/' -i /etc/ssh/sshd_config
+
+$a sed -re 's/^(PasswordAuthentication)([[:space:]]+)yes/\1\2no/' -i /etc/ssh/sshd_config
+$a sed -re 's/^(PermitRootLogin)([[:space:]]+)yes/\1\2no/' -i /etc/ssh/sshd_config
+$a sed -re 's/^(PermitRootLogin)([[:space:]]+)prohibit-password/\1\2no/' -i /etc/ssh/sshd_config
+
+#IPTABLES
+$a apt update
+$a apt install iptables-persistent htop -y
+$a iptables --policy INPUT DROP
+$a iptables -A INPUT -i lo -j ACCEPT
+$a iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+$a iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+$a netfilter-persistent save
+
+#SWAP
+$a fallocate -l 1G /swapfile
+$a chmod 600 /swapfile
+$a mkswap /swapfile
+$a swapon /swapfile
+$a cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | $a tee -a /etc/fstab
+echo 'vm.swappiness=10' | $a tee -a /etc/sysctl.conf
+echo 'vm.vfs_cache_pressure=50' | $a tee -a /etc/sysctl.conf
+
+#DOCKER
+$a apt-get install ca-certificates curl gnupg -y
+$a install -m 0755 -d /etc/apt/keyrings
+$a curl -fsSL https://download.docker.com/linux/debian/gpg | $a gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+$a chmod a+r /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | $a tee /etc/apt/sources.list.d/docker.list > /dev/null
+$a apt-get update
+$a apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+$a reboot
